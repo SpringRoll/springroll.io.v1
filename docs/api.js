046f27c255e6ec87ec732a28288103a7ed4dbe60
Andrew Smith <andrew.smith@redspace.com>
2018-09-27 15:17:56 -0300

    added v1

---
+++ docs/api.js
@@ -0,0 +1,239 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "PIXI.BitmapText",
+        "PIXI.Container",
+        "PIXI.InteractionManager",
+        "PIXI.Point",
+        "PIXI.Sector",
+        "PIXI.Text",
+        "createjs.Container",
+        "createjs.MovieClip",
+        "createjs.Point",
+        "createjs.SpriteSheet",
+        "springroll.AbstractDisplay",
+        "springroll.AbstractHint",
+        "springroll.Animator",
+        "springroll.AnimatorHint",
+        "springroll.AnimatorTimeline",
+        "springroll.Application",
+        "springroll.ApplicationOptions",
+        "springroll.ApplicationPlugin",
+        "springroll.AssetCache",
+        "springroll.AssetLoad",
+        "springroll.AssetManager",
+        "springroll.AssetSizes",
+        "springroll.BaseState",
+        "springroll.CacheManager",
+        "springroll.Captions",
+        "springroll.ColorAlphaTask",
+        "springroll.CombinedCallback",
+        "springroll.Debug",
+        "springroll.DebugOptions",
+        "springroll.DelayedCall",
+        "springroll.Enum",
+        "springroll.EnumValue",
+        "springroll.EventDispatcher",
+        "springroll.FlashArtImageTask",
+        "springroll.FunctionHint",
+        "springroll.FunctionTask",
+        "springroll.GroupHint",
+        "springroll.HintsPlayer",
+        "springroll.Languages",
+        "springroll.ListTask",
+        "springroll.LoadTask",
+        "springroll.Loader",
+        "springroll.LoaderItem",
+        "springroll.LoaderResult",
+        "springroll.PageVisibility",
+        "springroll.Positioner",
+        "springroll.Property",
+        "springroll.PropertyDispatcher",
+        "springroll.SavedData",
+        "springroll.ScaleImage",
+        "springroll.ScaleItem",
+        "springroll.ScaleManager",
+        "springroll.Sound",
+        "springroll.SoundContext",
+        "springroll.SoundInstance",
+        "springroll.SoundTask",
+        "springroll.State",
+        "springroll.StateEvent",
+        "springroll.StateManager",
+        "springroll.StringFilters",
+        "springroll.Task",
+        "springroll.TimeUtils",
+        "springroll.UIScaler",
+        "springroll.UserData",
+        "springroll.VOHint",
+        "springroll.VOPlayer",
+        "springroll.WeightedRandom",
+        "springroll.easeljs.Animator",
+        "springroll.easeljs.Application",
+        "springroll.easeljs.BasePanel",
+        "springroll.easeljs.BaseState",
+        "springroll.easeljs.BitmapMovieClip",
+        "springroll.easeljs.BitmapMovieClipInstance",
+        "springroll.easeljs.BitmapMovieClipTask",
+        "springroll.easeljs.BitmapTask",
+        "springroll.easeljs.BitmapUtils",
+        "springroll.easeljs.Button",
+        "springroll.easeljs.Cutscene",
+        "springroll.easeljs.CutsceneTask",
+        "springroll.easeljs.DisplayAdapter",
+        "springroll.easeljs.DragData",
+        "springroll.easeljs.DragManager",
+        "springroll.easeljs.EaselJSDisplay",
+        "springroll.easeljs.FlashArt",
+        "springroll.easeljs.FlashArtAtlasTask",
+        "springroll.easeljs.FlashArtTask",
+        "springroll.easeljs.FlashSpriteSheetTask",
+        "springroll.easeljs.MovieClipInstance",
+        "springroll.easeljs.ReversePlayback",
+        "springroll.easeljs.SoundButton",
+        "springroll.easeljs.SpriteSheetTask",
+        "springroll.easeljs.Texture",
+        "springroll.easeljs.TextureAtlas",
+        "springroll.easeljs.TextureAtlasTask",
+        "springroll.native.Circle",
+        "springroll.native.DisplayAdapter",
+        "springroll.native.Ellipse",
+        "springroll.native.NativeDisplay",
+        "springroll.native.Point",
+        "springroll.native.Polygon",
+        "springroll.native.Rectangle",
+        "springroll.pixi.AdvancedMovieClip",
+        "springroll.pixi.AdvancedMovieClipInstance",
+        "springroll.pixi.AdvancedMovieClipTask",
+        "springroll.pixi.Animator",
+        "springroll.pixi.BitmapFontTask",
+        "springroll.pixi.Button",
+        "springroll.pixi.DisplayAdapter",
+        "springroll.pixi.DragData",
+        "springroll.pixi.DragManager",
+        "springroll.pixi.ParallelSpineData",
+        "springroll.pixi.PixiDisplay",
+        "springroll.pixi.SpineAnimTask",
+        "springroll.pixi.SpineAtlas",
+        "springroll.pixi.SpineAtlasTask",
+        "springroll.pixi.SpineInstance",
+        "springroll.pixi.TextureAtlas",
+        "springroll.pixi.TextureAtlasTask",
+        "springroll.pixi.TextureTask",
+        "window.Array",
+        "window.Math",
+        "window.Number",
+        "window.Object",
+        "window.RequestUtils",
+        "window.String",
+        "window.extend",
+        "window.include",
+        "window.mixin",
+        "window.namespace"
+    ],
+    "modules": [
+        "Animation",
+        "Captions",
+        "Container Client",
+        "Core",
+        "Debug",
+        "EaselJS Animation",
+        "EaselJS Cutscene",
+        "EaselJS Display",
+        "EaselJS States",
+        "EaselJS UI",
+        "Hints",
+        "Languages",
+        "Native Display",
+        "PIXI Animation",
+        "PIXI Display",
+        "PIXI Spine",
+        "PIXI UI",
+        "Sound",
+        "States",
+        "UI"
+    ],
+    "allModules": [
+        {
+            "displayName": "Animation",
+            "name": "Animation"
+        },
+        {
+            "displayName": "Captions",
+            "name": "Captions"
+        },
+        {
+            "displayName": "Container Client",
+            "name": "Container Client"
+        },
+        {
+            "displayName": "Core",
+            "name": "Core"
+        },
+        {
+            "displayName": "Debug",
+            "name": "Debug"
+        },
+        {
+            "displayName": "EaselJS Animation",
+            "name": "EaselJS Animation"
+        },
+        {
+            "displayName": "EaselJS Cutscene",
+            "name": "EaselJS Cutscene"
+        },
+        {
+            "displayName": "EaselJS Display",
+            "name": "EaselJS Display"
+        },
+        {
+            "displayName": "EaselJS States",
+            "name": "EaselJS States"
+        },
+        {
+            "displayName": "EaselJS UI",
+            "name": "EaselJS UI"
+        },
+        {
+            "displayName": "Hints",
+            "name": "Hints"
+        },
+        {
+            "displayName": "Languages",
+            "name": "Languages"
+        },
+        {
+            "displayName": "Native Display",
+            "name": "Native Display"
+        },
+        {
+            "displayName": "PIXI Animation",
+            "name": "PIXI Animation"
+        },
+        {
+            "displayName": "PIXI Display",
+            "name": "PIXI Display"
+        },
+        {
+            "displayName": "PIXI Spine",
+            "name": "PIXI Spine"
+        },
+        {
+            "displayName": "PIXI UI",
+            "name": "PIXI UI"
+        },
+        {
+            "displayName": "Sound",
+            "name": "Sound"
+        },
+        {
+            "displayName": "States",
+            "name": "States"
+        },
+        {
+            "displayName": "UI",
+            "name": "UI"
+        }
+    ]
+} };
+});
